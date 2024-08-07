09df26f55eedc01c48328ba9a607c834d222370f
Taeyoung-Yang <110372475+Taeyoung-Yang>
2024-08-07 22:42:24 +0900

    first commit

---
+++ src/WildTurkey.java
@@ -0,0 +1,9 @@
+public class WildTurkey implements Turkey {
+    public void gobble() {
+        System.out.println("골골");
+    }
+
+    public void fly() {
+        System.out.println("짧은 거리를 날고 있어요!");
+    }
+}
